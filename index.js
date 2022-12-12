eeb2ebb3b02bf297d8566e7d18d48481489891db
Anna Koval <anny.koval@icloud.com>
2022-12-12 23:11:21 +0200

    added project

---
+++ index.js
@@ -0,0 +1,49 @@
+setInterval(displayTime, 1000);
+
+function displayTime() {
+
+    const timeNow = new Date();
+
+    let hoursOfDay = timeNow.getHours();
+    let minutes = timeNow.getMinutes();
+    let seconds = timeNow.getSeconds();
+    let currentDate = timeNow.getDate();
+    let months = [
+        "Jan",
+        "Feb",
+        "March",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ];
+      let month = months[timeNow.getMonth()];
+    let year = timeNow.getFullYear();
+    let period = "AM";
+
+    if (hoursOfDay > 12) {
+        hoursOfDay-= 12;
+        period = "PM";
+    }
+
+    if (hoursOfDay === 0) {
+        hoursOfDay = 12;
+        period = "AM";
+    }
+
+    hoursOfDay = hoursOfDay < 10 ? "0" + hoursOfDay : hoursOfDay;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    let time = hoursOfDay + ":" + minutes + ":" + seconds + period;
+
+    document.getElementById('clock').innerHTML = time;
+    document.getElementById('date').innerHTML = currentDate + " " + month  + "," + " " + year;
+
+}
+displayTime();
